62ce9cd8bc0adeae4b472d5369b53ef6dc064535
Ryan Morales <ryan.morales@west-mec.org>
2024-11-04 15:08:01 -0700

    Update manipulateData.js

--- manipulateData.js
+++ manipulateData.js
@@ -14,30 +14,6 @@ sharedEmitter.on('documentsFetched', (newDocuments) => {
     // Automatically save to file when documents are updated
     saveDocuments();
 });
-function readOutput() {
-    // Read the JSON file synchronously
-    try {
-        const data = fs.readFileSync(jsonFilePath, 'utf8');
-        const trimmedData = data.trim();
-        documents = JSON.parse(trimmedData);
-
-        if (documents.length > 0) {
-            console.log('First document:', documents[0]);
-        } else {
-            console.log('No documents found.');
-        }   // Emit the documents back to the system
-        sharedEmitter.emit('documentsLoaded', documents);
-        return documents;
-    } catch (err) {
-        console.error('Error reading or parsing JSON file:', err);
-    }
-}
-// Function to log document keys
-function logDocumentKeys(doc) {
-    for (let key in doc) {
-        console.log(`${key}: ${doc[key]}`);
-    }
-}
 
 // Getter function to retrieve documents
 function getDocuments() {
